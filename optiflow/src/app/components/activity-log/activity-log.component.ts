d39609125ff560b3e36925cc76d92e0c0d10ee35
TheNinthOne <olga.ivkovic@student.um.si>
2023-06-12 22:23:49 +0200

    finished implementing activity log page

--- optiflow/src/app/components/activity-log/activity-log.component.ts
+++ optiflow/src/app/components/activity-log/activity-log.component.ts
@@ -1,35 +1,67 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ActivityLog } from '@src/app/share/classes/activity-log.class';
+import { ActivityLogPagination } from '@src/app/share/classes/pagination-activity-log.class';
 import { AlertService } from '@src/app/share/services/alert.service';
 import { ActivityLogApiService } from '@src/app/share/services/api/activity-log-api.service';
 import { LoadingScreenService } from '@src/app/share/services/loading-screen.service';
 import { DxDataGridComponent } from 'devextreme-angular';
-import { Observable, finalize } from 'rxjs';
+import CustomStore from 'devextreme/data/custom_store';
+import { Observable, finalize, lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-activity-log',
   templateUrl: './activity-log.component.html',
   styleUrls: ['./activity-log.component.scss']
 })
-export class ActivityLogComponent implements OnInit{
+export class ActivityLogComponent {
   @ViewChild(DxDataGridComponent) grid$: DxDataGridComponent;
+  dataSource: any = {};
 
-  public activity$ = new Observable<ActivityLog[]>();
+  public activity$ = new Observable<ActivityLogPagination>();
+  currentPage: number = 1;
 
   constructor(
     private route: ActivatedRoute,
     private activityLogApiService: ActivityLogApiService,
-    private readonly loadingService: LoadingScreenService
-  ) {}
-
-  ngOnInit(): void {
-    this.refreshData();
+    private readonly loadingService: LoadingScreenService,
+    httpClient: HttpClient
+  ) {
+    function isNotEmpty(value: any): boolean {
+      return value !== undefined && value !== null && value !== '';
+    }
+    this.dataSource = new CustomStore({
+      key: 'id',
+      load(loadOptions: any) {
+        let body: any = { };
+        [
+          'take',
+          'skip',
+          'sort',
+          'filter',
+          'group',
+        ].forEach((i) => {
+          if (i in loadOptions && isNotEmpty(loadOptions[i])) {
+            body[i] = loadOptions[i];
+          }
+        });
+        body.user_id = 4;
+        return lastValueFrom(
+          httpClient.post('http://staging.gpc-hosting.com/api/activitylog', body)
+        ).then((data: any) => ({
+            data: data.data,
+            totalCount: data.total,
+          }))
+          .catch((error) => {
+            throw 'Data Loading Error';
+          });
+      },
+    });
   }
 
   private refreshData() {
     this.loadingService.startLoading();
-    this.activity$ = this.activityLogApiService.getActivities()
+    this.activity$ = this.activityLogApiService.getActivities(this.currentPage)
       .pipe(finalize(() => this.loadingService.stopLoading()));
   }
 }
--- optiflow/src/app/share/services/api/activity-log-api.service.ts
+++ optiflow/src/app/share/services/api/activity-log-api.service.ts
@@ -5,22 +5,24 @@ import { Observable} from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { DataApiService } from './templates/data-api.service';
 import { ActivityLog } from '../../classes/activity-log.class';
+import { ActivityLogPagination } from '../../classes/pagination-activity-log.class';
 
 @Injectable({
     providedIn: 'root'
 })
-export class ActivityLogApiService extends DataApiService<ActivityLog> {
+export class ActivityLogApiService extends DataApiService<ActivityLogPagination> {
 
     constructor(protected http: HttpClient) {
-        super(environment.api + '/activitylog', http, ActivityLog);
+        super(environment.api + '/activitylog', http, ActivityLogPagination);
     }
 
     // api/authenticates
 
-    getActivities(): Observable<ActivityLog[]> {
+    getActivities(page: number): Observable<ActivityLogPagination> {
         const params = {
+            page: page,
             user_id: 4
         };
-        return this.getAll(params);
+        return this.getOnePaginated(undefined,'',params);
     }
 }
